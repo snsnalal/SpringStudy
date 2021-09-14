5a02a89692bf788e3b33ee91ce77cd302273038a
JHN <jujujo79@gmail.com>
2021-09-14 13:44:10 +0900

    Initial commit

---
+++ src/test/java/seongho/seonghospring/SeonghoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package seongho.seonghospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeonghoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
